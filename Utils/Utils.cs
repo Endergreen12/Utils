face4062240720f6d86993d0d3913c60d5578349
Endergreen12 <endergreenjapan@gmail.com>
2024-08-01 16:16:42 +0900

    Add Utils class

---
+++ Utils/Utils.cs
@@ -0,0 +1,16 @@
+﻿namespace Utils
+{
+    public static class Utils
+    {
+        public static string newLineStr = Environment.NewLine;
+
+        public static void DisplayHelpIfArgIsHelp(string[] args, string helpText)
+        {
+            if (args.Length > 0 && args[0] == "--help")
+            {
+                Console.WriteLine(helpText);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
